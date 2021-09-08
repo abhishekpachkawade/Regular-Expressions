cf0c08604b1f08ef7e6231df31329765c551f0d2
abhishekpachkawade <abhishekpachkawade@gmail.com>
2021-09-08 13:07:13 +0530

    First Name Validation using Regex

--- UserRegistrationProgram.java
+++ UserRegistrationProgram.java
@@ -5,14 +5,12 @@ import java.util.regex.*;
 
 
 public class UserRegistrationProgram {
-	
-	static UserRegistrationProgram userRegistrationProgram = new UserRegistrationProgram();
 
-	//method Check whether first name entered is valid or not it should start with cap and end with last name
+	//method Check whether first name entered is valid or not it should start with cap and
 	//should have minimum 3 char    
 	private boolean checkFirstName(String firstName) {
 		//first letter should be cap and should have min 3 letters
-		String regex = "[A-Z]{1}[A-Za-z]{2}$";
+		String regex = "^[A-Z]{1}[A-Za-z]{2}";
 		
 		//Compile regular expression
 		Pattern pattern = Pattern.compile(regex);
@@ -26,92 +24,35 @@ public class UserRegistrationProgram {
 	        return m.matches();
 	}
 	
-	
-	
-	public boolean checkLastName(String lastName) {
-		//lastName letter should be cap and should have min 3 letters
-		String regex= "^[A-Z][a-z]{2,}";
-		
-		//Compile regular expression
-		Pattern pattern = Pattern.compile(regex);
-		
-		//if condition to check last name is null or not
-		if(lastName == null) {
-			return false;
-		}
-		
-		Matcher matcher = pattern.matcher(lastName);
-		
-		return matcher.matches();
-				
-	}
-	
-	
-	
-	public void userInput() {
-		Scanner scan = new Scanner(System.in);
-		
-		//ask to user input
-		System.out.println("Enter the first name");
-		//scanning an storing the user value
-		String firstName = scan.next();
-		
-		//ask to user input
-		System.out.println("Enter the last Name");
-		//scanning an storing the user value
-		String lastName = scan.next();
-		
-		//passing the value in side the validateDatails
-		userRegistrationProgram.validateDetails(firstName, lastName);
-	
-	}
-	
-	
-	
-	public void validateDetails(String firstName,String lastName) {
-		//check The condition to first name is valid or not
-		if (userRegistrationProgram.checkFirstName(firstName)) {
-			
-			System.out.println("User first Name is Valid ");
-			
-		}else {
-			
-			System.out.println("User first name is not Valid");
-		}
-		
-		
-		//check The condition to last name is valid or not
-		if (userRegistrationProgram.checkFirstName(lastName)) {
-			
-			System.out.println("User last Name is Valid ");
-			
-		}else {
-			
-			System.out.println("User last name is not Valid");
-		}		
-	}
-	
-	
 
 	public static void main(String[] args) {
+		//creating object of UserRegistrationProgram class
+		UserRegistrationProgram user = new UserRegistrationProgram();
 		
-		//calling method userInput
-		userRegistrationProgram.userInput();	
+		//ask to user value
+		System.out.println("Enter the User Name");
+		Scanner scan = new Scanner(System.in); 
+		//scanning and storing the value in firstName
+		String firstName = scan.next();
+		
+		//check The condition to First name is valid or not 
+		if(user.checkFirstName(firstName)) {
+			System.out.println("User Name is Valid ");
+		}else {
+			System.out.println("User name is not Valid");
+		}
 
 	}
-
+	
 }
 
 
-
 /*
-  OUTPUT 
-Enter the first name
+OUTPUT
+Enter the User Name
 Abh
-Enter the last Name
-Las
-User first Name is Valid 
-User last Name is Valid 
+User Name is Valid 
 
- */
- 
+*/
+
+
